8596b87dff2da5072db1eaf77e194cdaeefa5376
zeeshanhanif <zeeshanhanif@gmail.com>
2020-07-01 17:13:33 +0500

    updated ABI and contract address

--- client/src/contract/ExpenseTrackerContract.js
+++ client/src/contract/ExpenseTrackerContract.js
@@ -1,77 +1,94 @@
-export const EXPENSE_TRACKER_ADDRESS = "0x3f71b3bf5419c151a131d36c523333be5abdd10e";
+export const EXPENSE_TRACKER_ADDRESS = "0x5C799ad02CEc0ed43AFC6a5b3Ea62490754bd08C";
 
 export const EXPENSE_TRACKER_ABI = [
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "description",
-				"type": "string"
-			},
-			{
-				"internalType": "int64",
-				"name": "amount",
-				"type": "int64"
-			}
-		],
-		"name": "addTransaction",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "balance",
-		"outputs": [
-			{
-				"internalType": "int256",
-				"name": "",
-				"type": "int256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "transaction",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "transactionOwner",
-				"type": "address"
-			},
-			{
-				"internalType": "string",
-				"name": "transactionDescription",
-				"type": "string"
-			},
-			{
-				"internalType": "int64",
-				"name": "amount",
-				"type": "int64"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "transactionCount",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
-]
+    {
+      "inputs": [],
+      "name": "balance",
+      "outputs": [
+        {
+          "internalType": "int256",
+          "name": "",
+          "type": "int256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "transaction",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "transactionOwner",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "transactionDescription",
+          "type": "string"
+        },
+        {
+          "internalType": "int64",
+          "name": "amount",
+          "type": "int64"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "description",
+          "type": "string"
+        },
+        {
+          "internalType": "int64",
+          "name": "amount",
+          "type": "int64"
+        }
+      ],
+      "name": "addTransaction",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "transactionCount",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [],
+      "name": "getHello",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "pure",
+      "type": "function",
+      "constant": true
+    }
+  ]
